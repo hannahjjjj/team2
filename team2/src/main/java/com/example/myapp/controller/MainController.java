77ddf149ccd8fb891eef047a817e85adf4b1294a
minseonseon <ms9732@naver.com>
2023-08-02 15:56:09 +0900

    check

--- team2/src/main/java/com/example/myapp/controller/MainController.java
+++ team2/src/main/java/com/example/myapp/controller/MainController.java
@@ -1,13 +1,36 @@
 package com.example.myapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	ITeam2Service Team2Service;
 	
 	@RequestMapping("/")
 	public String main() {
 		return"index";
 	}
+	
+	
+	
+	
+	
+//	@RequestMapping(value = "/delete", method=RequestMethod.GET)
+//	public int deleteTeam() {
+//		
+//		return 0;
+//	}
+//	
+//	@RequestMapping(value = "/delete", method=RequestMethod.POST)
+//	public int deleteTeam() {
+//		
+//		return 0;
+//	}
+//	
+	
 }
