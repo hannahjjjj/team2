558097845d657d3fdd9d24166c7a680990fba024
hannahjjjj <wh467925@gmail.com>
2023-08-02 17:13:54 +0900

    CR

--- team2/src/main/java/com/example/myapp/controller/MainController.java
+++ team2/src/main/java/com/example/myapp/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.example.myapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,16 +9,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.myapp.model.Team2VO;
+import com.example.myapp.service.ITeam2Service;
+import com.example.myapp.service.Team2Service;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 public class MainController {
-	@Autowired
 	
+	@Autowired
+	ITeam2Service teamService;
 	
 	@RequestMapping("/")
 	public String main() {
 		return"index";
 	}
+	@RequestMapping(value="/create" , method=RequestMethod.GET)
+	public String createteam(Model model) {
+		List<Team2VO> teamList = teamService.selectAllTeam();
+//		teamList.stream().forEach((t) -> {
+//			System.out.println(t.getTeamname());
+//		});
+		model.addAttribute("teamList", teamList);
+		return "index";
+	}
 	
+	@RequestMapping(value="/create" , method=RequestMethod.POST)
+	public String createteam(Model model, Team2VO team) {
+		try {
+		team.setTeamno(team.getTeamno());
+		team.setTeamname(team.getTeamname());
+		team.setTeamage(team.getTeamage());
+		teamService.createTeam(team);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}		
+		return"index";
+	}
 
 }
--- team2/src/main/java/com/example/myapp/service/Team2Service.java
+++ team2/src/main/java/com/example/myapp/service/Team2Service.java
@@ -1,17 +1,28 @@
 package com.example.myapp.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.myapp.dao.ITeam2Repository;
 import com.example.myapp.model.Team2VO;
 
+@Service
 public class Team2Service implements ITeam2Service {
 	
 	@Autowired
-	ITeam2Service teamService;
+	ITeam2Repository teamRepository;
 	
 	@Override
 	public void createTeam(Team2VO team2) {
-		teamService.createTeam(team2);
+		teamRepository.createTeam(team2);
+	}
+
+	@Override
+	public List<Team2VO> selectAllTeam() {
+		return teamRepository.selectAllTeam();
 	}
 
 }
