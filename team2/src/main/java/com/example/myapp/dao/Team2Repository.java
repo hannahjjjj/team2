992c4b11173ea2e7ab04591b11a43eae5b2eb187
hannahjjjj <wh467925@gmail.com>
2023-08-02 15:54:35 +0900

    feat:interface

--- team2/src/main/java/com/example/myapp/dao/Team2Repository.java
+++ team2/src/main/java/com/example/myapp/dao/Team2Repository.java
@@ -1,5 +1,19 @@
 package com.example.myapp.dao;
 
-public class Team2Repository {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.myapp.model.Team2VO;
+
+@Repository
+public class Team2Repository implements ITeam2Repository {
+	
+	@Autowired
+	ITeam2Repository teamRepository;
+
+	@Override
+	public void createTeam(Team2VO team2) {
+		teamRepository.createTeam(team2);		
+	}
 
 }
--- team2/src/main/java/com/example/myapp/service/Team2Service.java
+++ team2/src/main/java/com/example/myapp/service/Team2Service.java
@@ -1,5 +1,17 @@
 package com.example.myapp.service;
 
-public class Team2Service {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.myapp.model.Team2VO;
+
+public class Team2Service implements ITeam2Service {
+	
+	@Autowired
+	ITeam2Service teamService;
+	
+	@Override
+	public void createTeam(Team2VO team2) {
+		teamService.createTeam(team2);
+	}
 
 }
